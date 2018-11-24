50f09dd2a744e557e7ad604c41708f48a8f79f22
vitalii <vitalii@dreaming.local>
2018-11-24 14:12:40 +0200

    add

--- mysite/settings.py
+++ mysite/settings.py
@@ -20,10 +20,11 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'b+dm8xtg(tld3lv9$@23y=b$%nq$s(h5nqynhqdj_v%p+8h_x^'
+
+from .secret_key import SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
